b934163049b9f71885209fa2d84eaffc9bdb8e97
sujitdebnath <47503518+sujitdebnath@users.noreply.github.com>
2025-08-17 22:36:46 +0000

    Deploying to gh-pages from @ sujitdebnath/sujitdebnath.github.io@372f88e97e0fdf13425df4f00f36b9aad66c19bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/photo-gallery/";
+            window.location.href = "/blog/2025/cheleta/";
           
         },
       },{id: "post-a-distill-style-blog-post",
